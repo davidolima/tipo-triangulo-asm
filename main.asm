2eb0ca2b76a7ddcdf7488758af599ba4e7bf3108
David <67931753+davidolima@users.noreply.github.com>
2023-06-16 22:45:37 -0300

    Calculo de distância e prints

--- main.asm
+++ main.asm
@@ -6,8 +6,20 @@ section .data
 	p2textlen  equ $-p2text
     p3text  db "Insira o terceiro ponto (x3, y3):",10,0
 	p3textlen  equ $-p3text
+
     pontosInseridos:  db "Pontos inseridos (p1, p2 e p3, respectivamente):",10,0
 	pontosInseridoslen:  equ $-pontosInseridos
+
+    equilatero  db "Os pontos formam um triângulo equilatero.",10,0
+	equilaterolen  equ $-equilatero
+    escaleno  db "Os pontos formam um triângulo escaleno.",10,0
+	escalenolen  equ $-escaleno
+    isosceles  db "Os pontos formam um triângulo isosceles.",10,0
+	isosceleslen  equ $-isosceles
+
+    dist:  db "Distância: %d",10,0
+    distlen: equ $-dist
+
     ponto:  db "(%d, %d)",10,0
 	pontotlen:  equ $-p3text
 
@@ -20,13 +32,27 @@ section .bss
     y2:  resb 8
     x3:  resb 8
     y3:  resb 8
-    d12:  resb 8
-    d23:  resb 8
-    d13:  resb 8
+    d12: resb 8
+    d23: resb 8
+    d13: resb 8
+    r:   resb 8
 
 section .text
 	global main
-    extern printf, scanf, pow
+    extern printf, scanf, sqrt
+
+%macro print 1
+    mov rdi, %1
+    mov rax, 0
+    call printf
+%endmacro
+
+%macro printValor 2
+    mov rdi, %2
+    mov rsi, %1
+    mov rax, 0
+    call printf
+%endmacro
 
 %macro printPonto 2
     mov rdi, ponto
@@ -36,10 +62,8 @@ section .text
 %endmacro
 
 %macro lerPonto 3
-    mov rdi, %3
-    mov rax, 0
-    call printf
-
+    print %3
+    
     mov     rdi, fm
     mov     rsi, %1
     mov     rax, 0
@@ -50,6 +74,28 @@ section .text
     mov     rax, 0
     call    scanf
 %endmacro
+
+calcularDistancia:
+    ;; espera que x1 esteja em r8 e y1 em r9
+    ;; espera que x2 esteja em r10 e y2 em r11
+    ;; sqrt((x2-x1)^2+(y2-y1)^2)
+    mov dword [r], 0                  ; zerar r
+
+    sub r8, r9                  ;x2-x1
+    sub r10, r11                ;y2-y1
+
+    imul r8, r8                 ;(x2-x1)^2
+    imul r10, r10               ;(y2-y1)^2
+
+    add r8, r10                 ;(x2-x1)^2+(y2-y1)^2
+    mov [r], r8
+
+    fld qword [r]
+    fsqrt                       ;sqrt((x2-x1)^2+(y2-y1)^2)
+    fstp qword [r]
+
+    ;; retorna em r
+    ret
 
 main:
     ;; Setar o rbp para a base da pilha
@@ -63,15 +109,81 @@ main:
     lerPonto x3, y3, p3text    ;; Ponto 3
 
     ;; Printar os pontos
-    mov rdi, pontosInseridos    ; "Pontos inseridos (p1, p2, p3):"
-    mov rax, 0
-    call printf
+    print pontosInseridos    ; "Pontos inseridos (p1, p2, p3):"
 
     printPonto x1, y1
     printPonto x2, y2
     printPonto x3, y3
 
+    ;; Calcular distância
+
+    mov r8, [x1]
+    mov r9, [y1]
+    mov r10, [x2]
+    mov r11, [y2]
+    call calcularDistancia
+    mov rax, [r]
+    mov [d12], rax
+
+    mov r8, [x1]
+    mov r9, [y1]
+    mov r10, [x3]
+    mov r11, [y3]
+    call calcularDistancia
+    mov rax, [r]
+    mov [d13], rax
+
+    mov r8, [x2]
+    mov r9, [y2]
+    mov r10, [x3]
+    mov r11, [y3]
+    call calcularDistancia
+    mov rax, [r]
+    mov [d23], rax
+
+    printValor d12, dist
+    printValor d13, dist
+    printValor d23, dist
+
+    ;; Avaliar tipo do triângulo
+    mov rax, d12
+    mov rbx, d13
+
+    cmp rax, rbx                ; d12 == d13?
+    jne doisLadosDiferentes
+
+    mov rbx, d23
+
+    cmp rax, rbx                ; d12 == d23?
+    je  isEquilatero            ; d12 == d13 == d23
+
+    jmp isIsosceles             ; d12 == d13 != d23
+
+doisLadosDiferentes:            ; d12 != d13
+    mov rcx, d23
+    cmp rax, rcx
+    jne d12_ne_d13_ne_d23
+
+    jmp isIsosceles             ; d13 != d12 == d23
+
+d12_ne_d13_ne_d23:
+    cmp rbx, rcx                ; d13 == d23?
+    jne isEscaleno              ; d12 != d13 != d23
+
+isEscaleno:
+    print escaleno
+    jmp finalizar
+
+isIsosceles:
+    print isosceles
+    jmp finalizar
+
+isEquilatero:
+    print equilatero
+    jmp finalizar
+
+finalizar:
     ;; Finalizar programa
+    mov rdi, [r]
     mov rax, 60
-    mov rdi, 0
     syscall
